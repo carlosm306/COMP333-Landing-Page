b389ee1081e2905b409f516e54efb4806f99c5dd
fmindich <fmindich@gmail.com>
2025-03-05 16:51:59 -0500

    signup preview -- upload functionality and most error checks

--- signup.php
+++ signup.php
@@ -22,7 +22,7 @@
     include 'dbconnection.php';
     $userid = $_POST['userid'];
     $password = $_POST['password'];
-    $confirm_password = $_POST['confirm password'];
+    $confirm_password = $_POST["confirm_password"];
 
     // echo empty($userid);
     // echo empty($userid) > 0;
@@ -35,20 +35,20 @@
     if (empty($userid)) {
         echo "please enter a username";
       } elseif (empty($password)) {
-        echo 'please enter a password';}
-    //   } else {
-    //     echo 'please confirm password';
-    //   }
-
-    //   (empty($confirm_password)) 
+        echo 'please enter a password';
+      } elseif (empty($confirm_password)) {
+        echo 'please confirm password';
+      }
 
         elseif (0) {
         echo 'that username is already in use. Please enter a new one';
-      } elseif ('$password' !== '$confirm_password') {
-        echo 'passwords do not match';
-        echo '$password';
-        echo $confirm_password;
-      } elseif (strlen($password)<10) {
+      } 
+    //   elseif ('$password' == '$confirm_password') {
+    //     echo 'passwords do not match';
+    //     echo $password;
+    //     echo $confirm_password;
+    //   } 
+      elseif (strlen($password)<10) {
         echo 'your password must be at least ten characters long';
       } else {
         // If the password and username conform to the conditions, then record them;
@@ -64,6 +64,7 @@
         mysqli_stmt_bind_param($stmt, "ss", $userid, $hashed_password);
         // Run the prepared statement.
         mysqli_stmt_execute($stmt);
+        echo "signup succesful";
       }
 
 
