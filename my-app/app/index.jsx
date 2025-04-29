d799e094d114e9e2fb99f10bdbdb67b5ce7614c4
cmunoz306 <cmunoz01@wesleyan.edu>
2025-04-29 12:26:54 -0400

    Changes to make app run and changed file names

---
+++ my-app/app/index.jsx
@@ -0,0 +1,80 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}>
+      <Text style={[styles.titletext, styles.space]}> AnimalMovies</Text>
+      <Text style={styles.text}> Watch and rate your favorite animal movies here, such as... </Text>
+      <Text style={styles.text}> Possible Image </Text>
+      <Link href="/signup" style={styles.button}> Click here to sign up </Link>
+      <Link href="/login" style={styles.button}> Click here to log in </Link>
+    </View>
+
+  );
+}
+
+const styles= StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "aquamarine",
+  },
+  titletext: {
+    flex: 1,
+    color: "black",
+    fontSize: 45 , 
+    alignItems: "center" , 
+    fontWeight: "bold"
+  },
+  text: {
+    flex : 3 ,
+    color: "black",
+    fontSize: 20,
+    alignItems: "center" , 
+    justifyContent: "center",
+  }, 
+  space: {
+    marginBottom: 40 , 
+  },
+  button: {
+    fontSize: 20 , 
+    textDecorationLine: "underline",
+    color: "purple",
+  },
+});
+
+/*
+import { BrowserRouter } from "react-router-dom";
+import Reviews from "./readreview" ; 
+import Addreview from "./addreview";
+import Updatereview from "./updatereview";
+
+function App() {
+  return (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="/readreview" element={<Reviews/>}/>
+        <Route path="/addreview" element={<Addreview/>}/>
+        <Route path="/updatereview" element={<Updatereview/>} />
+      </Routes>
+    </BrowserRouter>
+
+
+
+
+  </div>
+
+
+  )
+
+
+
+}
+  */
---
+++ my-app/app/updatereview.jsx
@@ -0,0 +1,85 @@
+
+
+import { Link } from "expo-router";
+import { Text, View, TextInput, Button, Alert } from "react-native";
+import { StyleSheet } from "react-native";
+import React from "react"
+import { useState } from "react" ; 
+import { NavigationContainer } from "@react-navigation/native";
+import {axios} from "axios"
+
+export default function Updatereview () {
+    const [Review, setReview] = useState({
+        movie: "",
+        rating: null,
+        review: "",
+    }) ; 
+
+    const Insertdata = () => {
+        if ((Review.rating == null) || (Review.movie =="") || (Review.review == "")) 
+            return Alert.alert("Please fill out all fields") ;
+        else {
+        try{
+          axios.put("http://localhost/index.php/user/list?limit=20", review)
+        }
+        catch{
+            console.log(err)
+            }
+    }
+    }
+    return (
+        <View className="form">
+            <Text style={styles.titletext}> Update review </Text>
+            <TextInput type="text" placeholder="movie" onChange={setReview} name="movie" /> 
+            <TextInput type="number" placeholder="rating" onChange={setReview} name="rating" />
+            <TextInput type="text" placeholder="review" onChange={setReview} name="review" />
+            <Button title= "Add" onPress={() => Insertdata}/>  
+        </View>
+        
+    ) ; 
+
+} ; 
+
+const styles= StyleSheet.create({
+    container : {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "aquamarine",
+    },
+    titletext: {
+      color: "black",
+      fontSize: 50 , 
+      alignItems: "center" , 
+      fontWeight: "bold",
+  
+    },
+    text: {
+      color: "black",
+      fontSize: 20,
+      alignItems: "center" , 
+      justifyContent: "center",
+    }, 
+    space: {
+      marginBottom: 40 , 
+    },
+    input: {
+        fontsize: 30,
+        color: "black",
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "black",
+        borderWidth: 3,
+        padding: 5,
+        margin: 5,
+    },
+    background: { 
+      flex: 1,
+      backgroundColor: "aquamarine"
+    },
+    button: {
+      fontSize: 20 , 
+      textDecorationLine: "underline",
+      color: "purple",
+    },
+  });
