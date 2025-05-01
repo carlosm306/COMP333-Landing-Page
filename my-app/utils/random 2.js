2a0a5cdc96a726a891ce52ebe675c626db0dccde
cmunoz306 <cmunoz01@wesleyan.edu>
2025-04-30 22:42:23 -0400

    web app

---
+++ my-app/utils/random 2.js
@@ -0,0 +1,35 @@
+import { Dimensions } from "react-native";
+
+const windowHeight = Dimensions.get("window").height
+const windowWidth = Dimensions.get("window").width
+
+export const getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+export const getPipeSizePosPair = (addToPosX = 0) => {
+    let yPosTop = -getRandom(300, windowHeight - 50)
+
+    const gap = 200; // Adjustable pipe gap
+
+    const pipeTop = {
+    pos: { x: windowWidth + addToPosX, y: yPosTop },
+    size: { height: windowHeight * 2, width: 75 }
+  };
+
+    const pipeBottom = {
+    pos: {
+      x: windowWidth + addToPosX,
+      y: windowHeight * 2 + gap + yPosTop
+    },
+    size: { height: windowHeight * 2, width: 75 }
+  };
+
+    /*
+    const pipeTop = { pos: {x : windowWidth + addToPosX, y:yPosTop}, 
+    size: {height: windowHeight * 2, width: 75}}
+    const pipeBottom = { pos: {x : windowWidth + addToPosX, y: windowHeight * 2 + 200 + yPosTop}, 
+    size: {height: windowHeight * 2, width: 75}}
+    */
+
+    return {pipeTop, pipeBottom}
+}
